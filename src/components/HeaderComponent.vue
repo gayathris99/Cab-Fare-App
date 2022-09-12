b5c8f60216239450b0b5a47c34482d2b740a0400
gayathris99 <gayathri11499@gmail.com>
2022-09-12 15:21:29 +0530

    Deleted Quasar Designs and added basic header

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="header-component q-pa-md">
+    <div class="text-whitesmoke fw-700 fs-32">RideX</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderComponent",
+};
+</script>
+
+<style lang="scss" scoped>
+.header-component {
+  background-color: #202020;
+}
+</style>
